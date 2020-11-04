ee96a6870b3320612f307e6814337aa24ad9d4d3
Yuta Okuma <y.dream.nest@icloud.com>
2020-11-04 18:36:57 +0900

    slick.jsを追加

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$('.slick01').slick({
+  // 自動再生。trueで自動再生される。
+  autoplay: true,
+  // 自動再生で切り替えるを時間
+  autoplaySpeed: 10000,
+  // 切り替えのアニメーション。ease, linear, ease-in, ease-out, ease-in-out
+  cssEase: 'ease-in-out',
+  // 左右の次へ、前へボタンを表示するかどうか
+  arrows: false
+});
